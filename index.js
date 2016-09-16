0be24af9304b2232d9d78a46d899e4c05f6d7bb3
mrbadduck <eric.mirowitz@gmail.com>
2016-09-15 19:11:36 -0500

    ugh

--- index.js
+++ index.js
@@ -35,11 +35,6 @@ function updateTime()
         document.body.style.backgroundColor = "#" + h + m + s;
     }
 
-    var height = window.innerHeight;
-    height = (height/2)-125;
-    /*document.getElementById("hex").style.paddingTop = height + "px";*/
-
-
     setTimeout(function ()
     {
         updateTime();
